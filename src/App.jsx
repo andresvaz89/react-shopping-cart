36705c8b3863e0f5294b87b513b8c7d311f5caae
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-02-05 23:10:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Shop from './pages/shop/Shop';
+import Cart from './pages/cart/Cart';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
