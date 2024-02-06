48ed364473518d19b846fab42a9ecdae2c7e9ebf
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-02-06 22:40:26 +0100

    bug fixed in Shop while importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
+import { Navbar } from './components/navbar';
 import { Shop } from './pages/shop/Shop';
 import { Contact } from './pages/contact';
-import { Cart } from './pages/cart/Cart';
+import { Cart } from './pages/cart/cart';
 import { ShopContextProvider } from './context/shop-context';
 
 function App() {
